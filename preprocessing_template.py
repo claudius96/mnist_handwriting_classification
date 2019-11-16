7b8faa009350ba87cccb588c5f94aba7d11e7cfe
Dingani Muzimba <39743045+claudius96@users.noreply.github.com>
2019-11-16 21:32:05 +0100

    Delete preprocessing_template.py

--- preprocessing_template.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
-import warnings
-warnings.filterwarnings('ignore')
-
-#importing dataset
-dataset = pd.read_csv('train.csv')
